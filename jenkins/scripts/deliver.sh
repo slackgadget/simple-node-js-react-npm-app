320266be510c2c7f94ceaca034dd8133801e0838
Chris <cjwebster@gmail.com>
2020-11-04 16:01:22 +0000

    deploy script and required changes

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 echo 'The following "npm" command builds your Node.js/React application for'
 echo 'production in the local "build" directory (i.e. within the'
@@ -9,21 +9,13 @@ set -x
 npm run build
 set +x
 
-echo 'The following "npm" command runs your Node.js/React application in'
-echo 'development mode and makes the application available for web browsing.'
-echo 'The "npm start" command has a trailing ampersand so that the command runs'
-echo 'as a background process (i.e. asynchronously). Otherwise, this command'
-echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
-echo 'is followed by another command that retrieves the process ID (PID) value'
-echo 'of the previously run process (i.e. "npm start") and writes this value to'
-echo 'the file ".pidfile".'
-set -x
-npm start &
-sleep 1
-echo $! > .pidfile
-set +x
+#Remove any existing local container
+docker container rm -f webster
 
-echo 'Now...'
-echo 'Visit http://localhost:3000 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
-echo 'created your initial Pipeline as a Jenkinsfile.)'
+pwd
+
+#Build the image -- nb. Set the path by proptery or ENV Variable
+docker image build ../../ -t simple-node-js-react-app:LATEST
+
+#Run the image in the container expose Port 3000
+docker run -p 3000:3000/tcp --name=webster  simple-node-js-react-app:LATEST 
--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -3,7 +3,7 @@
 'echo ************** current TEST  path is *******************'
 echo $PATH
 'echo *********************************************************'
-PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/Users/christopherwebster/springsource/apache-maven-3.0.4/bin:/usr/local/git/bin:/usr/local/bin/npm:/Users/christopherwebster/Dev/google-cloud-sdk/bin:
+PATH=$PATH:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:/usr/local/git/bin:/usr/local/bin/npm:
 node -v
 
 
