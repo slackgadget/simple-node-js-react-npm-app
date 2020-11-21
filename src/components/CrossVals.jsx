e5152d661625cf032ced3934c375390c188b4e87
Chris <cjwebster@gmail.com>
2020-11-21 16:12:04 +0000

    Update CrossVals.jsx

--- src/components/CrossVals.jsx
+++ src/components/CrossVals.jsx
@@ -9,9 +9,9 @@ const CrossVals = () => {
       try {
         let response = await fetch('http://localhost:8989/crossvals');
         response = await response.json();
-        // eslint-disable-next-line no-console
         setCrossValInfo(response);
-        console.log(response)
+        // eslint-disable-next-line no-console
+        console.log(response);
       } catch (e) {
         // eslint-disable-next-line no-console
         console.error(e.message);
