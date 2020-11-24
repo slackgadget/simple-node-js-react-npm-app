44ac0c4c7bd805851172c17068d41e459ec6a6d6
Chris <cjwebster@gmail.com>
2020-11-24 15:41:15 +0000

    Update CrossVals.jsx

--- src/components/CrossVals.jsx
+++ src/components/CrossVals.jsx
@@ -90,7 +90,11 @@ const CrossVals = () => {
               </Table.HeaderCell>
               <Table.HeaderCell colSpan="5">
                 <Menu floated="left" pagination>
-                  <Menu.Item as="a" onClick={() => setCrossValServiceURL(crossValInfo._links.first.href)}>
+                  <Menu.Item as="a" onClick={() => {
+                    if(!crossValInfo._links.first.href){
+                      setCrossValServiceURL(crossValInfo._links.first.href)
+                    }
+                  }}>
                     First
                   </Menu.Item>
                   <Menu.Item as="a" onClick={() => setCrossValServiceURL(crossValInfo._links.prev.href)} icon>
